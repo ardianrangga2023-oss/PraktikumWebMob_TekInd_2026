cb4194481fabbacb0ace187652e848c97f18aaff
ArdianRangga <ardianrangga.2023@student.uny.ac.id>
2026-03-27 20:22:10 +0700

    Add files via upload

---
+++ Pertemuan6_23051430033_Ardian Rangga Smara Dhana Prahita/latihan2.js
@@ -0,0 +1,9 @@
+suhuMesin.addEventListener("mouseover", function() {
+    suhuMesin.style.color = "lightblue";
+    suhuMesin.style.fontWeight = "bold";
+});
+
+suhuMesin.addEventListener("mouseout", function() {
+    suhuMesin.style.color = "black";
+    suhuMesin.style.fontWeight = "normal";
+});
